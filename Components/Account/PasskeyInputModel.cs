9d90d35ec2f3b1a18d1a70ed076e0e8a2bcab1ca
Raul-Sabin Petrut <raul.petrut44@yahoo.com>
2026-03-29 12:24:06 +0300

    initial commit - Blazor Web App starter template

---
+++ Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace WeddingPlannerApp.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error { get; set; }
+}
---
+++ Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace WeddingPlannerApp.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
