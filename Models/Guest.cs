e93a2c1081864e8919c9114cd756650217e65a63
Raul-Sabin Petrut <raul.petrut44@yahoo.com>
2026-04-13 20:53:03 +0300

    Replace unnecessary RspvStatus table with Enum type and modified the type of some fields to Enum

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -3,22 +3,51 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WeddingPlannerApp.Models;
 
+public enum RsvpStatus
+{
+    Pending,
+    Declined,
+    Confirmed
+}
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other
+}
+
+public enum Group
+{
+    Friends,
+    Family,
+    Colleagues,
+    Other
+}
+
+public enum DietaryRequirements
+{
+    None,
+    Vegan,
+    Vegetarian,
+    GlutenFree,
+    Halal,
+    Kosher,
+    Other
+}
+
 public class Guest
 {
     [Key]
-    public int GuestId { get; set; }
+    public int GuestId { get; init; }
     
-    public int EventId { get; set; }
+    public int EventId { get; init; }
     [Required, ForeignKey(nameof(EventId))]
-    public required Event Event { get; set; }   
+    public required Event Event { get; init; }   
     
     public int? TableId { get; set; }
     [ForeignKey(nameof(TableId))]
     public WeddingTable? WeddingTable { get; set; }
-    
-    public int RsvpStatusId { get; set; }
-    [Required, ForeignKey(nameof(RsvpStatusId))]
-    public required RsvpStatus RsvpStatus { get; set; }
     
     [Required]
     public required string FirstName { get; set; }
@@ -26,14 +55,20 @@ public class Guest
     [Required]
     public required string LastName { get; set; }
     
-    public string? Phone { get; set; }
-    public string? Email { get; set; }
-    public string? Category { get; set; }
     public int? Age { get; set; }
-    public string? Gender { get; set; }
-    public string? DietaryRequirements { get; set; }
+    
+    [Required]
+    public required string Email { get; init; }
+    
+    public string? Phone  { get; set; }
+    public Gender? Gender { get; set; }
+    
+    public RsvpStatus           RsvpStatus          { get; set; } = RsvpStatus.Pending;
+    public Group                Group               { get; set; } = Group.Other;
+    public DietaryRequirements  DietaryRequirements { get; set; } = DietaryRequirements.None;
+    
     public string? Notes { get; set; }
     
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; init; } = DateTime.UtcNow;
 }
