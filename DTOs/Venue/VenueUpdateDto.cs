fefc17dfae8c7555d83d876fabfa5eac7e48684c
Raul-Sabin Petrut <raul.petrut44@yahoo.com>
2026-05-06 15:27:13 +0300

    Created DTOs for Venue model

---
+++ DTOs/Venue/VenueUpdateDto.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WeddingPlannerApp.DTOs.Venue;
+
+public sealed class VenueUpdateDto
+{
+    [Required]
+    [MaxLength(64)]
+    public required string Name { get; set; }
+
+    [Required]
+    [MaxLength(64)]
+    public required string Address { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int MinCapacity { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int MaxCapacity { get; set; }
+
+    [Range(typeof(decimal), "0", "999999.99")]
+    public decimal EstimatedPrice { get; set; }
+}
