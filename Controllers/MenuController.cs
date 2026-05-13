e6c85c35e2b03f020115a57d564e969e20a1bf93
lily7878 <goreadenisa@gmail.com>
2026-05-13 17:57:50 +0300

    Solved datatbase migraton error + added button automatic gests placing

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeddingPlannerApp.Data;
+using WeddingPlannerApp.Models;
+
+namespace WeddingPlannerApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MenusController(ApplicationDbContext context) : ControllerBase
+{
+    // GET: api/Menus
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await context.Menus.ToListAsync());
+    }
+
+    // POST: api/Menus
+    [HttpPost]
+    public async Task<IActionResult> Create()
+    {
+        var menu = new Menu
+        {
+            Name = "Premium Wedding Menu",
+            Description = "Luxury 4 course wedding dinner",
+            Price = 350
+        };
+
+        context.Menus.Add(menu);
+        await context.SaveChangesAsync();
+
+        return Ok(menu);
+    }
+}
